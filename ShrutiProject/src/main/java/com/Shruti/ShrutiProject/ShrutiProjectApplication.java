747095592845fb8362ceb689c393cd2186ba1f03
shruti dhanwade <shrutidhanawade1998@gmail.com>
2024-03-20 11:52:58 +0530

    adding files

---
+++ ShrutiProject/src/main/java/com/Shruti/ShrutiProject/ShrutiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Shruti.ShrutiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrutiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrutiProjectApplication.class, args);
+	}
+
+}
---
+++ ShrutiProject/src/test/java/com/Shruti/ShrutiProject/ShrutiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shruti.ShrutiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrutiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
